caae3d05d4ca13449dfc7b8e3284c7f385226b29
josueJURE <josue.jure@gmail.com>
2023-08-26 08:30:56 +0100

    feat(js): add 1 row to grid layout use querySelectorAll to display characters on screen

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const output = document.querySelector(".output");
 const btns = document.querySelectorAll(".child");
+let screen = Array.from(document.querySelectorAll(".screen"))[0];
 
 let operator = ''
 let data = [];
 
 btns.forEach(btn => {
     btn.addEventListener("click", function(e) {
-        console.log("josue")
         let buttonText = e.target.innerText;
+        console.log(buttonText)
+        screen.innerText = buttonText;
 
         if(buttonText === "AC") {
             output.innerText = "";
