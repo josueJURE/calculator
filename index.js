e392f5b00a6ed880c1e0f11fb8ce643b1dd1b30c
josueJURE <josue.jure@gmail.com>
2023-08-26 12:27:19 +0100

    feat(js): when the user clicks the +/- button, the number displayed on the screen will toggle between positive and negative values.

--- index.js
+++ index.js
@@ -1,22 +1,32 @@
 const btns = document.querySelectorAll("[data-child]");
 let screen = document.querySelector("[data-screen]");
 
-let operator = ''
+let operator = "";
 let data = [];
+let digit;
 
-btns.forEach(btn => {
-    btn.addEventListener("click", function(e) {
-        let buttonText = e.target.innerText;
-        console.log(buttonText)
-        screen.innerText = buttonText;
+btns.forEach((btn) => {
+  btn.addEventListener("click", function (e) {
+    let buttonText = e.target.innerText;
 
-        if(buttonText === "AC") {
-            output.innerText = "";
-            data = [];
-            operator = undefined
-        }
-    })
+    if (buttonText === "AC") {
+      screen.innerText = "";
+      data = [];
+      operator = undefined;
+    } else if (!isNaN(buttonText)) {
+      screen.innerText = buttonText;
+    } else if (buttonText === "+/-") {
+      digit = parseFloat(screen.innerText);
+      screen.innerText = -digit; // Toggle the sign of the digit
+    }
+  });
 });
+
+
+
+
+
+
 
 
 
