6c8a5b76fef39629d23c3646810d21b58482f2a6
josueJURE <josue.jure@gmail.com>
2023-08-27 10:37:25 +0100

    feat(js): add regex in first else if conditional so that operators can be displayed onto the screen

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const btns = document.querySelectorAll("[data-child]");
 let screen = document.querySelector("[data-screen]");
+const operators = document.querySelectorAll("[data-operator]");
 
-let operator = "";
+console.log(operators)
+
+
+
+let x = "";
 let data = [];
 let digit;
 
@@ -12,12 +17,16 @@ btns.forEach((btn) => {
     if (buttonText === "AC") {
       screen.innerText = "";
       data = [];
-      operator = undefined;
-    } else if (!isNaN(buttonText)) {
+      x = undefined;
+    } else if (!isNaN(buttonText) || /[\/x\-+]/.test(buttonText)
+    ) {
       screen.innerText += buttonText;
     } else if (buttonText === "+/-") {
       digit = parseFloat(screen.innerText);
       screen.innerText = -digit; // Toggle the sign of the digit
+    } else if (buttonText === ".") {
+        if(!screen.innerText.includes("."))
+        screen.innerText += "."
     }
   });
 });
