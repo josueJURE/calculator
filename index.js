c7435e8e076a0d071be4da8030874cec96e1a2a7
josueJURE <josue.jure@gmail.com>
2023-12-19 15:46:45 +0000

    feat(JS) refactor canUserAddDot(buttonValue)

--- index.js
+++ index.js
@@ -40,9 +40,9 @@ btns.forEach((btn) => {
       toggleSign();
     }
 
-    if (buttonValue === ".") {
-      canUserAddDot();
-    }
+
+    canUserAddDot(buttonValue);
+  
 
     if (buttonValue === "=") {
       try {
@@ -119,26 +119,28 @@ function deteLastEntry() {
   }
 }
 
-function canUserAddDot() {
-  var dotAllowed = true;
-  for (var i = data.length - 1; i >= 0; i--) {
-    console.log("data > " + data[i]);
-    if (data[i] === ".") {
-      dotAllowed = false;
-      break;
-    } else if (/[\/*\-+]/.test(data[i])) {
-      break;
+function canUserAddDot(button) {
+  if(button === ".") {
+    var dotAllowed = true;
+    for (var i = data.length - 1; i >= 0; i--) {
+      console.log("data > " + data[i]);
+      if (data[i] === ".") {
+        dotAllowed = false;
+        break;
+      } else if (/[\/*\-+]/.test(data[i])) {
+        break;
+      }
     }
-  }
-  if (dotAllowed) {
-    if (data.length == 0) {
-      data.push("0");
-    } else if (/[\/*\-+]/.test(data[data.length - 1])) {
-      data.push("0");
+    if (dotAllowed) {
+      if (data.length == 0) {
+        data.push("0");
+      } else if (/[\/*\-+]/.test(data[data.length - 1])) {
+        data.push("0");
+      }
+      data.push(".");
     }
-    data.push(".");
+    screen.innerText = data.join(" ");
   }
-  screen.innerText = data.join(" ");
 }
 
 function deleteEverythingFromScreen(button) {
