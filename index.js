c9ddbf3dca9c5ca946680bc65b1f437f8d093aef
josueJURE <josue.jure@gmail.com>
2024-03-01 15:05:16 +0000

    (feat:JS): remove unused parameter

--- index.js
+++ index.js
@@ -294,7 +294,7 @@ function clearHistoryInLocalStorage() {
   localStorage.removeItem("calculatorHistory");
 }
 
-function togglesClearHistoryButton(element, btn) {
+function togglesClearHistoryButton(element) {
   clearHistoryBtn.classList.toggle("display", element.childElementCount > 0);
 }
 // functions creations ends
